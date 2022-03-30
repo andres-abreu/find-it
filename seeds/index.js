9eab5c92e10388a5bccf950baca9bb81d7210963
Maria Andrea Rodriguez <mandrea.rodriguezf@gmail.com>
2022-03-30 19:56:15 -0400

    added userData.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,14 @@
 const sequelize = require('../config/connection');
-const seedCategory = require('./categoryData')
+const seedCategory = require('./categoryData');
+const seedUserData = require('/userData')
 
 
 const seedAll = async () => {
     await sequelize.sync({force: true})
 
     await seedCategory();
+    await seedUserData();
+    
     console.log('\n----- CATEGORIES SEEDED -----\n');
 
     process.exit(0)
