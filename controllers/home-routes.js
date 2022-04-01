e2668f4f39679c727da51955bdfefb8e5c5f1a2e
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-03-31 23:06:02 -0400

    update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const {User, Products, Category} = require('../models')
+const { User, Products, Category, Product } = require('../models')
 
 const withAuth = require('../utils/auth')
 
@@ -9,8 +9,8 @@ const withAuth = require('../utils/auth')
 router.get('/', async (req, res) => {
     try {
         const dbCategoryData = await Category.findAll()
-        const categories = dbCategoryData.map((category) => category.get({plain: true}))
-        res.render('homepage',{
+        const categories = dbCategoryData.map((category) => category.get({ plain: true }))
+        res.render('homepage', {
             categories,
             loggedIn: req.session.loggedIn
         })
@@ -24,9 +24,50 @@ router.get('/login', (req, res) => {
     res.render('login')
 })
 
-router.get('/createproduct', (req, res) =>{
+router.get('/createproduct', (req, res) => {
     res.render('productform')
 })
+
+// GET one Categories for user
+router.get('/category/:id', async (req, res) => {
+    try {
+        const dbCategoryData = await Category.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Product,
+                    attributes: [
+                        'id',
+                        'title',
+                        'description',
+                        'image',
+                        'price',
+                    ],
+                },
+            ],
+        });
+
+        const category = dbCategoryData.get({ plain: true });
+        res.render('category', { category });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET one product
+router.get('/product/:id', async (req, res) => {
+    try {
+        const dbProductData = await Product.findByPk(req.params.id);
+
+        const product = dbProductData.get({ plain: true });
+
+        res.render('product', { product });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 
 
 module.exports = router;
