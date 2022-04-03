3a882f09a558690a14bd6988bd49d7101e3e7917
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-04-03 17:35:58 -0400

    update

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -21,6 +21,26 @@ router.post('/', async (req, res) => {
   }
 });
 
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // Login
 router.post('/login', async (req, res) => {
   try {
