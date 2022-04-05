599274e68602093e4bc435ad1713c4e5c35ba03a
Maria Andrea Rodriguez <mandrea.rodriguezf@gmail.com>
2022-04-04 21:56:23 -0400

    merge conflict nav bar

--- seeds/categoryData.js
+++ seeds/categoryData.js
@@ -2,16 +2,16 @@ const {Category} = require('../models')
 
 const categoryData = [
     {
-        category_name: 'Shirts',
+        category_name: 'Tops',
     },
     {
-        category_name: 'Shorts'
+        category_name: 'Bottoms'
     },
     {
-        category_name: 'Skirts'
+        category_name: 'Shoes'
     },
     {
-        category_name: 'Hats'
+        category_name: 'Accesories'
     },
     {
         category_name: 'Dress'
